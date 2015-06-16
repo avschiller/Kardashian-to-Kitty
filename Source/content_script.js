ccd0579ae1bca60c12ef6c8efb243b8f1dc8df50
avschiller <adamschiller@Adams-MacBook-Air-3.local>
2015-06-16 00:11:00 -0700

    i made it

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,18 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	// v = v.replace(/\bThe Cloud\b/g, "My Butt");
+	// v = v.replace(/\bThe cloud\b/g, "My butt");
+	// v = v.replace(/\bthe Cloud\b/g, "my Butt");
+	// v = v.replace(/\bthe cloud\b/g, "my butt");
+
+	v = v.replace(/\bKardashian\b/g, "Kitty");
+	v = v.replace(/\bkardashian\b/g, "kitty");
+	
+	v = v.replace(/\bKim Kardashian\b/g, "Kitty Kat");
+	v = v.replace(/\bkKourtney Kardashian\b/g, "Meow Kitty");
+	v = v.replace(/\bKhloe Kardashian\b/g, "Purrrrr Kitty");
+	v = v.replace(/\bKhloé Kardashian\b/g, "Purrrrr Kitty");
 	
 	textNode.nodeValue = v;
 }
